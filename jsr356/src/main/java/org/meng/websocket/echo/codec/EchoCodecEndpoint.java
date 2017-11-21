bb34c5f3c6295e926c9a264cd15837c3498c7290
Meng Sun <sunmeng90@126.com>
2017-11-21 22:53:27 +0800

    1. Re-write last commit message about return value from onMessage annotated method:
    If the return type of the method annotated with @OnMessage is not void, the WebSocket implementation will send the return value to the other peer. The following code snippet returns the received text message in capitals back to the sender:
    
             @OnMessage
             public String myOnMessage (String txt) {
                return txt.toUpperCase();

--- jsr356/src/main/java/org/meng/websocket/echo/codec/EchoCodecEndpoint.java
+++ jsr356/src/main/java/org/meng/websocket/echo/codec/EchoCodecEndpoint.java
@@ -9,6 +9,16 @@ import java.io.IOException;
         encoders = {PersonEncoder.class})
 public class EchoCodecEndpoint {
 
+    /**
+     *
+     * If the return type of the method annotated with @OnMessage is not void, the WebSocket implementation will send
+     * the return value to the other peer. The following code snippet returns the received text message in capitals back to the sender:
+
+     @OnMessage
+     public String myOnMessage (String txt) {
+     return txt.toUpperCase();
+
+     */
     @OnMessage
     public void onMessage(Person person, Session session) {
 
